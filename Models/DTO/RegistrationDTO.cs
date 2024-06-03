33a259a9e28f16de7c03ec5ffcd23784a7668101
Luc Juneau <kxtnluc@gmail.com>
2024-06-03 13:24:00 -0400

    initial-commit

---
+++ Models/DTO/RegistrationDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Questionnaire.Models.DTOs;
+
+public class RegistrationDTO
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string UserName { get; set; }
+}
---
+++ Models/DTO/UserProfileDTO.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Questionnaire.Models.DTOs;
+
+public class UserProfileDTO
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string UserName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+
+    public string IdentityUserId { get; set; }
+
+    public IdentityUser IdentityUser { get; set; }
+
+}
